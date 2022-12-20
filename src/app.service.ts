9bc1aa363889d4e6b59547c111c7c69e1b2aeef9
daanaerts <daanaerts@gmail.com>
2022-12-20 10:45:33 +0100

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import whichPolygon from 'which-polygon';
+import toGeoJson from '@mapbox/togeojson';
+import { readFileSync, writeFileSync } from 'fs';
+import { DOMParser } from 'xmldom';
+
+console.time('load countries JSON');
+const countryFile = readFileSync('./src/countries.geojson', 'utf-8');
+const countryJSON = JSON.parse(countryFile);
+const queryCountry = whichPolygon(countryJSON);
+console.timeEnd('load countries JSON');
+
+console.time('load pcz KML to JSON');
+const pczFile = readFileSync('./src/pcz.geojson', 'utf-8');
+const pczJson = JSON.parse(pczFile);
+const queryPCZ = whichPolygon(pczJson);
+
+console.log(pczJson.features[0]);
+console.log(countryJSON.features[0]);
+
+console.timeEnd('load pcz KML to JSON');
+
+@Injectable()
+export class AppService {
+  lookup(coordinates: string): string {
+    const [long, lat] = coordinates.split(',');
+
+    const country = queryCountry([lat, long]);
+    const pcz = queryPCZ([lat, long]);
+
+    console.log(pcz);
+    return { ...country, ...pcz };
+  }
+}
